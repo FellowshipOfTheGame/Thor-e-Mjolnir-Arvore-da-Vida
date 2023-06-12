8b9ece16d0eead3c32ac581b91fe8e40cb4d9094
lucas castro <lucas_o__castro@hotmail.com>
2023-06-12 01:28:19 -0300

    Sistema inicial de rendering de dano dos blocos

--- Assets/Scripts/ThorGame/HittableBlock.cs
+++ Assets/Scripts/ThorGame/HittableBlock.cs
@@ -1,20 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace ThorGame
 {
     [RequireComponent(typeof(Rigidbody2D))]
-    public class HittableBlock : MonoBehaviour, IHittable
+    public class HittableBlock : MonoBehaviour, IHittable, IHealthProvider
     {
+        [field: SerializeField]
+        public int MaxHealth { get; private set; }
+        
         [SerializeField] private float breakHitMinSpeed;
-        [SerializeField] private int breakHitCount = 5;
         [SerializeField] private UnityEvent breakEvent;
+        
+        public int Health { get; private set; }
+        public event IHealthProvider.HealthUpdateEvent OnHealthChanged;
 
         private int _hitCount;
         private Rigidbody2D _rb;
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
+            Health = MaxHealth;
         }
 
         public void Hit(Vector2 point, Vector2 velocity)
@@ -22,8 +29,9 @@ namespace ThorGame
             if (velocity.sqrMagnitude < breakHitMinSpeed * breakHitMinSpeed) return;
             
             _rb.AddForceAtPosition(velocity, point);
-            _hitCount++;
-            if (_hitCount >= breakHitCount) Break();
+            Health--;
+            OnHealthChanged?.Invoke(Health + 1, Health);
+            if (Health <= 0) Break();
         }
 
         public void Break()
---
+++ Assets/Scripts/ThorGame/IHealthProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace ThorGame
+{
+    public interface IHealthProvider
+    {
+        delegate void HealthUpdateEvent(int oldHealth, int newHealth);
+        event HealthUpdateEvent OnHealthChanged;
+
+        int MaxHealth { get; }
+        int Health { get; }
+    }
+}
