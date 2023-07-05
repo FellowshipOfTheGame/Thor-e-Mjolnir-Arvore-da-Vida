922d008d314a6db0c8392f71ba19522b55886f11
AleLocci <alelocci@ime.usp.br>
2023-07-05 18:37:38 -0300

    Flecha elfo
    Implementada.

--- Assets/Scripts/Arrow/ArrowFire.cs
+++ Assets/Scripts/Arrow/ArrowFire.cs
@@ -9,14 +9,12 @@ public class ArrowFire : MonoBehaviour
     public Transform thor;
     public Transform arrow;
     public Transform elfo;
-    float dist;
-    Vector3 viewPosArrow;
-    Vector3 target;
     public Camera mainCam;
+    int ajusteY = 2;
+    float dist;
+    Vector3 viewPosArrow;    
     bool fire = false;
-    int speed = 0;
-    float smooth;
-    float angle;
+    int speed = 3;
     void Start()
     {
         
@@ -28,67 +26,35 @@ public class ArrowFire : MonoBehaviour
         
         dist = Vector3.Distance(thor.position, elfo.position);        
         
-        if (dist < 8 && !fire) 
-        {           
-            
-            if(arrow.position.y > thor.position.y && arrow.position.x < thor.position.x)
+       if (dist < 20) 
+       {
+            if (!fire)
             {
-                angle = 180 - Vector3.Angle(arrow.transform.position, thor.position);
+                arrow.transform.position = new Vector3(elfo.position.x, elfo.position.y + ajusteY, 0);
+                fire = true;
             }
-            else
-            {
-                if(arrow.position.y < thor.position.y && arrow.position.x < thor.position.x)
-                {
-                    angle = Vector3.Angle(arrow.transform.position, thor.position);
-                }
-                else
-                {
-                    if (arrow.position.y < thor.position.y && arrow.position.x > thor.position.x)
-                    {
-                        angle =  -Vector3.Angle(arrow.transform.position, thor.position) ;
-                    }
-                    else
-                    {
-                        if (arrow.position.y > thor.position.y && arrow.position.x > thor.position.x)
-                        {
-                            angle = 180 + Vector3.Angle(arrow.transform.position, thor.position);
-                        }
-                    }
-                }                
-            }
-            arrow.transform.position = elfo.position;
-            arrow.Rotate(0, 0, angle);
-            speed = 10;
-            target = thor.position;
-            fire = true;
-            smooth =  1.0f - Mathf.Pow(0.5f, Time.deltaTime * speed);
-            StartCoroutine(Example());
-        } 
-
-        arrow.position = Vector3.Lerp(arrow.position, target, smooth);
+            arrow.transform.position = arrow.transform.position + new Vector3(-speed * Time.deltaTime, 0, 0);
+       }       
         
+       if(dist < 2)
+        {
+
+            arrow.transform.position = new Vector3(100, 100, 0);
+            speed = 0;
+        }
 
         if (!(viewPosArrow.x >= 0 && viewPosArrow.x <= 1 && viewPosArrow.y >= 0 && viewPosArrow.y <= 1))
         {
-            // Destroy(arrow.transform.gameObject);
-            arrow.transform.position = elfo.position;
+            arrow.transform.position = new Vector3(elfo.position.x, elfo.position.y + ajusteY, 0);
         }
 
-    }
-
-    IEnumerator Example()
-    {       
-        yield return new WaitForSeconds(2);
-        //Destroy(arrow.transform.gameObject);
-        arrow.transform.position = elfo.position;
     }
 
     void OnCollisionEnter2D(Collision2D collision2D)
     {       
         if (collision2D.transform.tag.Equals("Thor"))
         {
-            //Destroy(arrow.transform.gameObject);
-            arrow.transform.position = elfo.position;
+            arrow.transform.position = new Vector3(elfo.position.x, elfo.position.y + ajusteY, 0);
         }
     }
 }
