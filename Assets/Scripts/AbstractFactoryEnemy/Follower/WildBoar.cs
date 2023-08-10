a4f6310ff7fa6259ba6f5385ad3695ea961eb7eb
AleLocci <alelocci@ime.usp.br>
2023-08-10 19:07:39 -0300

    Criando placar
    O placar foi inserido no javali, Cada vez que ele for atingido pelo display ocorre perda de pontos de vida.

--- Assets/Scripts/AbstractFactoryEnemy/Follower/WildBoar.cs
+++ Assets/Scripts/AbstractFactoryEnemy/Follower/WildBoar.cs
@@ -8,11 +8,15 @@ public class WildBoar : AFollower
     // Start is called before the first frame update
     public Transform thor;
     public Transform boar;
+    public Transform lifeBar;
+    float dano = 1.0f;
+    Vector3 scaleChange;
     int speed = -4;
     int life;
     void Start()
     {
-        life = lifeWildBoarBite;
+        life = lifeWildBoarBite/10;
+        scaleChange = new Vector3(dano, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -23,17 +27,18 @@ public class WildBoar : AFollower
 
     void OnCollisionEnter2D(Collision2D collision2D)
     {
-        if (collision2D.transform.tag.Equals("Thor"))
-        {
+        if (collision2D.transform.CompareTag("Thor"))
+        {           
             speed = -speed;
-            StartCoroutine(Back());
+            StartCoroutine(Back());           
         }
 
-        if (collision2D.transform.tag.Equals("hammer"))
+        if (collision2D.transform.CompareTag("hammer"))       
         {
-            life -= 10;//deve ser passada pelo thor
+            life -= 1;
+            lifeBar.transform.localScale -= scaleChange;
 
-            if(life <= 0)
+            if (life <= 0)
             {
                 Destroy(boar.gameObject);
             }
