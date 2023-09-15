bb1fe52b34a22e934c93bda98bd06a68d184e116
lucas castro <lucas_o__castro@hotmail.com>
2023-09-15 11:18:15 -0300

    EnemyAttacker e vida de entidade básicos

---
+++ Assets/Scripts/ThorGame/EntityHealth.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace ThorGame
+{
+    public class EntityHealth: MonoBehaviour, IHealthProvider
+    {
+        public event IHealthProvider.HealthUpdateEvent OnHealthChanged;
+
+        [SerializeField] private int _maxHealth;
+        public int MaxHealth => _maxHealth;
+
+        private int _health;
+
+        public int Health
+        {
+            get => _health;
+            set
+            {
+                int oldHealth = _health;
+                _health = Mathf.Clamp(value, 0, _maxHealth);
+
+                if (Health <= 0) Die();
+                OnHealthChanged?.Invoke(oldHealth, _health);
+            }
+        }
+
+        public void Damage(int dmg)
+        {
+            Health -= dmg;
+        }
+
+        public void Die()
+        {
+            if (_health > 0) _health = 0;
+            Destroy(gameObject);
+        }
+    }
+}
