64369c99a2500cac21c11386274692f7e10ccd2a
LucasOCastro <lucascastro@usp.br>
2023-09-26 22:15:17 -0300

    Bounce quando defende flecha

--- Assets/Scripts/ThorGame/AI/Enemy/RedirectableProjectile.cs
+++ Assets/Scripts/ThorGame/AI/Enemy/RedirectableProjectile.cs
@@ -5,21 +5,21 @@ namespace ThorGame.AI.Enemy
     [RequireComponent(typeof(Projectile), typeof(Rigidbody2D))]
     public class RedirectableProjectile : MonoBehaviour, IHittable
     {
+        [SerializeField] private float bounce;
+        [SerializeField] [Range(0,1)] private float hammerVelBounceWeight = 1f;
         private Rigidbody2D _rb;
         private Projectile _projectile;
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
-            _rb.constraints = RigidbodyConstraints2D.FreezeAll;
             _projectile = GetComponent<Projectile>();
         }
 
         public void Hit(Vector2 point, Vector2 velocity, int damage)
         {
             _projectile.enabled = false;
-            _rb.constraints = RigidbodyConstraints2D.None;
             _rb.velocity = Vector2.zero;
-            _rb.AddForce(velocity, ForceMode2D.Impulse);
+            _rb.AddForce(-bounce * _projectile.Direction + (velocity * hammerVelBounceWeight), ForceMode2D.Impulse);
         }
 
         bool IHittable.RequireMinSpeed => false;
---
+++ Assets/Scripts/ThorGame/AI/Tree/Nodes/Leaf/CanShootNode.cs
@@ -0,0 +1,19 @@
+﻿using ThorGame.AI.Enemy;
+
+namespace ThorGame.AI.Tree.Nodes.Leaf
+{
+    public class CanShootNode : TargetedNode
+    {
+        private EnemyRangedWeapon _ranged;
+        protected override void Init(AIData data)
+        {
+            base.Init(data);
+            _ranged = data.Runner.GetComponent<EnemyRangedWeapon>();
+        }
+
+        protected override NodeResult Process(AIData data)
+        {
+            return _ranged.CanShoot(Target.position) ? NodeResult.Success : NodeResult.Failure;
+        }
+    }
+}
