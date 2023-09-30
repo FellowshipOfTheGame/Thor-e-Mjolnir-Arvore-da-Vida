5d04c3b9989cadb9e48d8de07c94eb9738bb5191
lucas castro <lucas_o__castro@hotmail.com>
2023-09-30 17:15:32 -0300

    CanShoot error fixed

--- Assets/Scripts/ThorGame/AI/Tree/Nodes/Leaf/ShootNode.cs
+++ Assets/Scripts/ThorGame/AI/Tree/Nodes/Leaf/ShootNode.cs
@@ -7,6 +7,7 @@ namespace ThorGame.AI.Tree.Nodes.Leaf
     {
         private EnemyRangedWeapon _ranged;
         private Collider2D _targetCol;
+        private bool _couldShoot;
         protected override void Init(AIData data)
         {
             base.Init(data);
@@ -14,11 +15,13 @@ namespace ThorGame.AI.Tree.Nodes.Leaf
             _targetCol = Target.GetComponent<Collider2D>();
             
             var target = _targetCol.bounds.center;
-            _ranged.Shoot(target);
+            _couldShoot = _ranged.CanShoot(target);
+            if (_couldShoot) _ranged.Shoot(target);
         }
 
         protected override NodeResult Process(AIData data)
         {
+            if (!_couldShoot) return NodeResult.Failure;
             return _ranged.IsShooting ? NodeResult.Running : NodeResult.Success;
         }
     }
